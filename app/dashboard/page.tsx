2521d2ac654d59828b8fe4cae67bc0025280cdcb
Yingzhe Li <yingzheli@yingzhes-mbp.lan>
2024-02-03 18:21:08 -0800

    init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1 className=" text-xl">Dashboard page</h1>
+            <p>Hello, this is the Dashboard home page</p>
+        </div>
+    )
+  }
+  
---
+++ app/discover/page.tsx
@@ -0,0 +1,7 @@
+export default function Discover() {
+    return (
+        <div className="text-xl text-green-400">
+            Discover
+        </div>
+    )
+}
